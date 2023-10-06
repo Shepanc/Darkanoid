08d3f4fea3e7ccfb585e28e9b0b942773e921247
polten0 <alexey.mikryukov@mail.ru>
2023-10-06 22:08:18 +0300

    AddClassObject

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import pyray
 
+# Глобальные поля
 fps = 0
+items = []
+
+
+def update():
+    pass
+
+def draw():
+    pyray.begin_drawing()
+    pyray.end_drawing()
 
 def main():
     w, h = 800, 600
     pyray.init_window(w, h, 'test1')
 
     while not pyray.window_should_close():
-        pyray.begin_drawing()
-        pyray.end_drawing()
+        if fps % 60 == 0:
+            update()
+            draw()
     pyray.close_window()
 
 if __name__ == '__main__':
