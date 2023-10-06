c3b898add72a1dd61484149fd8ece96fb13759f9
polten0 <alexey.mikryukov@mail.ru>
2023-10-06 23:00:21 +0300

    SartPlease

--- Classes/Object.py
+++ Classes/Object.py
@@ -1,3 +1,9 @@
-class Object:
+from abc import ABC, abstractmethod
+class Object(ABC):
     def __init__(self, position):
         self.position = position
+
+    @abstractmethod
+    def draw(self):
+        pass
+
